f368550af76d88db90fdd4a02d241cd4bbeacbe1
Duyen Nguyen <davichannel023@gmail.com>
2024-06-28 10:17:24 +0700

    enemie done

--- Assets/Script/Enemy_Controllers.cs
+++ Assets/Script/Enemy_Controllers.cs
@@ -1,23 +1,60 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 
-public class Enemy_Movement : MonoBehaviour
+public class Enemy_Controllers : MonoBehaviour
 {
     private float speed = 2f;
     private int facingDirection = 1;
     private EnemyState enemyState;
 
     private Rigidbody2D rb;
-    public Transform player; 
-    private Animator anim;
+    public Transform player;
+    public Animator anim;
 
+    bool isAlive = true;
+    public float Health
+    {
+        set
+        {
+            if (value < _health)
+            {
+                anim.SetTrigger("hit");
+            }
+
+            _health = value;
+
+
+            if (value <= 0)
+            {
+                anim.SetBool("isAlive", false);
+                Destroy(gameObject, 1);
+            }
+        }
+        get
+        {
+            return _health;
+        }
+    }
+
+    public float _health = 3;
+
+    public Transform attackPoint;
+    public LayerMask playerLayers;
+
+    private float attackRange = 0.95f;
+    public int attackDamage = 20;
+
+    //thoi gian tan cong
+    private float attackRate = 2f;//2
+    float nextAttackTime = 0f;//0
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        anim.SetBool("isAlive", isAlive);
         ChangeState(EnemyState.Idle);
     }
 
@@ -33,7 +70,18 @@ public class Enemy_Movement : MonoBehaviour
             }
             Vector2 direction = (player.position - transform.position).normalized;
             rb.velocity = direction * speed;
+
+            if (Time.time >= nextAttackTime)
+            {
+                Attack();
+                nextAttackTime = Time.time + 3f / attackRate;
+            }
         }
+    }
+
+    void OnHit(float damage)
+    {
+        Health -= damage;
     }
 
     void Flip()
@@ -51,7 +99,34 @@ public class Enemy_Movement : MonoBehaviour
                 player = collision.transform;
             }
             ChangeState(EnemyState.Chasing);
-        } 
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Finish")
+        {
+            OnHit(1);
+        }
+    }
+    void Attack()
+    {
+        //play an animation 
+        anim.SetTrigger("Attack");
+        //enemy in range in attack
+        Collider2D[] hitPlayer = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, playerLayers);
+
+        //damage them 
+        foreach (Collider2D player in hitPlayer)
+        {
+            player.GetComponent<demoplayerWenemy>().TakeDamage(attackDamage);
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
@@ -81,5 +156,4 @@ public enum EnemyState
 {
     Idle,
     Chasing,
-
 }
--- Assets/Script/demoplayerWenemy.cs
+++ Assets/Script/demoplayerWenemy.cs
@@ -9,35 +9,53 @@ public class demoplayerWenemy : MonoBehaviour
 
     public Rigidbody2D rb;
 
+    public int maxHealth = 100;
+    public int cunrrentHealth;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        cunrrentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        cunrrentHealth -= damage;
+        //play hurt animation
+        //animator.SetTrigger("Hurt");
+
+        if (cunrrentHealth <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        //Debug.Log("Enemy died!");
+        //die animation
+        //animator.SetBool("isDead", true);
+        //disable the enemy
+        this.enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+        Destroy(gameObject, 0.2f);
+    }
     private void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        if( horizontal > 0 && transform.localScale.x < 0 ||
+        if (horizontal > 0 && transform.localScale.x < 0 ||
             horizontal < 0 && transform.localScale.x > 0)
         {
             Flip();
         }
-        rb.velocity = new Vector2 (horizontal, vertical) * speed;
+        rb.velocity = new Vector2(horizontal, vertical) * speed;
     }
+
 
     void Flip()
     {
         facingDirection *= -1;
         transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
