ea1b49cd458dd2601b506d640b81b8acf9b9b9d2
MyNameIsBonnie <bonniedebunny12345@gmail.com>
2024-07-12 17:04:51 +0700

    Revert "Merge branch 'main' of https://github.com/Cetacea0510/NhomLigma"
    This reverts commit 85254b5f61eaec32157525a211c3b272dd2fe69c, reversing
    changes made to 344f36ea4324004bca2bafcc702b674de1c9ad4c.

--- Assets/Script/demoplayerWenemy.cs
+++ Assets/Script/demoplayerWenemy.cs
@@ -1,43 +1,34 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class demoplayerWenemy : MonoBehaviour
 {
     private float speed = 5f;
+    public int facingDirection = 1;
+
     public Rigidbody2D rb;
 
     public int maxHealth = 100;
-    public int currentHealth;
+    public int cunrrentHealth;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentHealth = maxHealth;
-
-        // Kiểm tra và gán Rigidbody2D nếu chưa gán trong Inspector
-        if (rb == null)
-        {
-            rb = GetComponent<Rigidbody2D>();
-            if (rb == null)
-            {
-                Debug.LogError("Rigidbody2D component is missing from this game object!");
-            }
-        }
+        cunrrentHealth = maxHealth;
     }
 
     public void TakeDamage(int damage)
     {
-        currentHealth -= damage;
+        cunrrentHealth -= damage;
         //play hurt animation
         //animator.SetTrigger("Hurt");
 
-        if (currentHealth <= 0)
+        if (cunrrentHealth <= 0)
         {
             Die();
         }
     }
-
     void Die()
     {
         //Debug.Log("Enemy died!");
@@ -48,16 +39,23 @@ public class demoplayerWenemy : MonoBehaviour
         GetComponent<Collider2D>().enabled = false;
         Destroy(gameObject, 0.2f);
     }
-
     private void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector2 movement = new Vector2(horizontal, vertical).normalized;
-        if (rb != null)
+        if (horizontal > 0 && transform.localScale.x < 0 ||
+            horizontal < 0 && transform.localScale.x > 0)
         {
-            rb.velocity = movement * speed;
+            Flip();
         }
+        rb.velocity = new Vector2(horizontal, vertical) * speed;
+    }
+
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 }
