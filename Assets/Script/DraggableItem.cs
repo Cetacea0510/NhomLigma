6b535112af767cdd7ee31e8cc22f5b5df5bac237
MyNameIsBonnie <bonniedebunny12345@gmail.com>
2024-07-27 17:32:47 +0700

    .
    .

--- Assets/Script/DraggableItem.cs
+++ Assets/Script/DraggableItem.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
 using Image = UnityEngine.UI.Image;
