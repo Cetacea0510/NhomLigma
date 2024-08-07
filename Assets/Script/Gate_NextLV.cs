62751981bee6a409eaa80111e673f9fb01115e0a
Duyen Nguyen <davichannel023@gmail.com>
2024-08-07 12:36:18 +0700

    nextmap(chuaxong:0)

--- Assets/Script/Gate_NextLV.cs
+++ Assets/Script/Gate_NextLV.cs
@@ -1,18 +1,18 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Gate_NextLV : MonoBehaviour
 {
-    public int sceneBuildIndex;
+    public string sceneToLoad; // Tên của scene mà bạn muốn chuyển đến
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if(other.tag == "Player")
-        {
-            Debug.Log("next scene to" + sceneBuildIndex);
-            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
-        }
-    }
+     private void OnTriggerEnter2D(Collider2D other)
+     {
+         if(other.tag == "Player")
+         {
+             Debug.Log("next scene to" + sceneToLoad);
+             SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
+         }
+     }
 }
